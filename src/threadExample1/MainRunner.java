8c11045ca217daea3cd405bb52d100dd14f89b52
oskarbay01 <aidosuly0@gmail.com>
2022-08-31 22:00:54 +0600

    creation by implementing the Runnable interface

--- src/threadExample1/MainRunner.java
+++ src/threadExample1/MainRunner.java
@@ -2,15 +2,25 @@ package threadExample1;
 
 public class MainRunner {
     public static void main(String[] args) {
-        System.out.println("!!!!!!");
+        System.out.println("!!!!!! "+Thread.currentThread().getName());
         MyThread myThread = new MyThread();
         myThread.start();
+
+        Thread thread = new Thread(new MyThreadRunnable());
+        thread.start();
     }
 }
 
 class MyThread extends Thread{
     @Override
     public void run(){
-        System.out.println("Hello from MyThread");
+        System.out.println("Hello from MyThread "+Thread.currentThread().getName());
+    }
+}
+
+class MyThreadRunnable implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("Hello from MyThreadRunnable "+Thread.currentThread().getName());
     }
 }
